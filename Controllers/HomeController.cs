71cf86f9e9b9516a5fee2ad7e6b05f725d638c33
tamim36 <tamim36@student.sust.edu>
2020-01-19 11:55:02 +0600

    initial Commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ExampleProject.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExampleProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IStudentRepository _studentRepository;
+
+        public HomeController(IStudentRepository studentRepository)
+        {
+            _studentRepository = studentRepository;
+        }
+        public string index()
+        {
+            return _studentRepository.GetStudent(1).Name;
+        }
+    }
+}
---
+++ Models/MockStudentRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExampleProject.Models
+{
+    public class MockStudentRepository : IStudentRepository
+    {
+        private List<Student> _studentList;
+
+        public MockStudentRepository()
+        {
+            _studentList = new List<Student>()
+            {
+                new Student(){Id=1, Name="abc", Department="CSE"},
+                new Student(){Id=2, Name="tamim", Department="SWE"},
+                new Student(){Id=3, Name="arefin", Department="EEE"}
+            };
+        }
+
+        public Student GetStudent(int Id)
+        {
+            return _studentList.FirstOrDefault(e => e.Id == Id);
+        }
+    }
+}
